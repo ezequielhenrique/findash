fa7a90ed0f95645329ed9d1f348d72e540266f37
ezequielhenrique <ezequielhenrique806@gmail.com>
2025-01-28 15:30:21 -0300

    get fundamentals data

--- app.py
+++ app.py
@@ -17,10 +17,11 @@ stock_select = st.selectbox('Selecione a empresa', tickers.get_tickers_id())
 end_date = datetime.today()
 start_date = end_date - timedelta(days=5*365)
 
+ticker = None
 stock_data = None
 
 if stock_select:
-    ticker = id_to_ticker(stock_select)
+    ticker = id_to_ticker(stock_select, sa=False)
     stock_data = StockProcessor(ticker)
     stock_data.set_stock_data(period='1d', start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
 
@@ -34,11 +35,39 @@ col4.metric(label='Segmento', value=tickers.get_segmento(id_to_ticker(stock_sele
 
 section1 = st.container()
 col1, col2 = section1.columns([3, 1])
+
+# subcontainer = col1.container(border=True)
+# subcontainer.subheader(f'Cotação do {id_to_ticker(stock_select, sa=False)} - Últimos 5 anos')
+# subcontainer.line_chart(stock_data.get_closes_prices(), x_label='Data', y_label='Preço (R$)')
+# subcontainer.subheader(f'Volume do {id_to_ticker(stock_select, sa=False)} - Últimos 5 anos')
+# subcontainer.bar_chart(stock_data.get_volume(), color='#FF6400')
+
 subcontainer = col1.container(border=True)
-subcontainer.subheader(f'Cotação do {id_to_ticker(stock_select, sa=False)} - Últimos 5 anos')
-subcontainer.line_chart(stock_data.get_closes_prices(), x_label='Data', y_label='Preço (R$)')
-subcontainer.subheader(f'Volume do {id_to_ticker(stock_select, sa=False)} - Últimos 5 anos')
-subcontainer.bar_chart(stock_data.get_volume(), color='#FF6400')
+tab1, tab2, tab3 = subcontainer.tabs(['5 anos', '3 meses', '1 mês'])
+
+with tab1:
+    st.subheader(f'Cotação do {id_to_ticker(stock_select, sa=False)} - Últimos 5 anos')
+    st.line_chart(stock_data.get_closes_prices(), x_label='Data', y_label='Preço (R$)')
+    st.subheader(f'Volume do {id_to_ticker(stock_select, sa=False)} - Últimos 5 anos')
+    st.bar_chart(stock_data.get_volume(), color='#FF6400')
+
+with tab2:
+    stock_data = StockProcessor(ticker)
+    start_date = end_date - timedelta(days=90)
+    stock_data.set_stock_data(period='1d', start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
+    st.subheader(f'Cotação do {id_to_ticker(stock_select, sa=False)} - Últimos 3 meses')
+    st.line_chart(stock_data.get_closes_prices(), x_label='Data', y_label='Preço (R$)')
+    st.subheader(f'Volume do {id_to_ticker(stock_select, sa=False)} - Últimos 3 meses')
+    st.bar_chart(stock_data.get_volume(), color='#FF6400')
+
+with tab3:
+    stock_data = StockProcessor(ticker)
+    start_date = end_date - timedelta(days=30)
+    stock_data.set_stock_data(period='1d', start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
+    st.subheader(f'Cotação do {id_to_ticker(stock_select, sa=False)} - Últimos mês')
+    st.line_chart(stock_data.get_closes_prices(), x_label='Data', y_label='Preço (R$)')
+    st.subheader(f'Volume do {id_to_ticker(stock_select, sa=False)} -Últimos mês')
+    st.bar_chart(stock_data.get_volume(), color='#FF6400')
 
 subcontainer = col2.container(border=True)
 subcontainer.subheader('Dados Financeiros')
@@ -46,14 +75,14 @@ a, b = subcontainer.columns(2)
 a.metric(label='Captalização de Mercado', value=f'R$ {format_num(stock_data.get_market_cap())}')
 b.metric(label='Volume Médio', value=f'R$ {format_num(stock_data.get_average_volume())}')
 a, b = subcontainer.columns(2)
-a.metric(label='P/L', value='R$ 150,00')
-b.metric(label='P/VP', value='R$ 90,00')
+a.metric(label='P/L', value=stock_data.get_pl())
+b.metric(label='P/VP', value=stock_data.get_pvp())
 a, b = subcontainer.columns(2)
-a.metric(label='DY', value='R$ 150,00')
-b.metric(label='ROI', value='R$ 90,00')
+a.metric(label='DY', value=stock_data.get_dy())
+b.metric(label='ROI', value=stock_data.get_roe())
 a, b = subcontainer.columns(2)
-a.metric(label='Min. 52 Semanas', value='R$ 150,00')
-b.metric(label='Máx. 52 Semanas', value='R$ 90,00')
+a.metric(label='Min. 52 Semanas', value=f'R$ {stock_data.get_min_52_sem():.2f}')
+b.metric(label='Máx. 52 Semanas', value=f'R$ {stock_data.get_max_52_sem():.2f}')
 
 subcontainer = col2.container(border=True)
 subcontainer.subheader('Indicadores Técnicos')
@@ -70,4 +99,4 @@ b.metric(label='MA 100 dias', value='R$ 90,00')
 # st.dataframe(stock_data.get_stock_prices())
 # st.dataframe(stock_data.get_info())
 
-st.markdown('Fonte dos dados: **Yahoo Finance API**')
+st.markdown('Fonte dos dados: **Yahoo Finance API**, **Fundamentus api**')
--- data_processors/stock_processor.py
+++ data_processors/stock_processor.py
@@ -1,11 +1,13 @@
 import yfinance as yf
+import fundamentus
 
 
 class StockProcessor:
     def __init__(self, ticker):
         self.ticker = ticker
-        self.stock_data = yf.Ticker(ticker)
+        self.stock_data = yf.Ticker(ticker + '.SA')
         self.stock_prices = None
+        self.fundamental_data = fundamentus.get_papel(ticker)
 
     def set_stock_data(self, period, start, end):
         self.stock_prices = self.stock_data.history(period=period, start=start, end=end)
@@ -36,3 +38,21 @@ class StockProcessor:
 
     def get_average_volume(self):
         return self.stock_data.info['averageVolume']
+
+    def get_pl(self):
+        return float(self.fundamental_data['PL'][0]) / 100
+
+    def get_pvp(self):
+        return float(self.fundamental_data['PVP'][0]) / 100
+
+    def get_dy(self):
+        return self.fundamental_data['Div_Yield'][0]
+
+    def get_roe(self):
+        return self.fundamental_data['ROE'][0]
+
+    def get_max_52_sem(self):
+        return float(self.fundamental_data['Max_52_sem'][0])
+
+    def get_min_52_sem(self):
+        return float(self.fundamental_data['Min_52_sem'][0])
